14cef05900ed382514f9dabe2649b07f1e17a603
salmanrafiq <salmanrafiq5839@gmail.com>
2020-01-18 21:02:16 +0500

    Assignment 7

---
+++ calculator/src/lib.rs
@@ -0,0 +1,16 @@
+pub mod sim_calculator{
+    pub fn calculate1(no1:u32,no2:u32)
+      {
+    let mut add= no1 + no2;
+println!("Add numbers : {}",add);
+let mut mul= no1 * no2;
+println!("Add numbers : {}",mul);
+let mut div= no1 / no2;
+println!("Add numbers : {}",div);
+      }
+    pub fn calculator2(no1:u32,no2:u32)
+    {
+        let mut sub= no2-no1;
+        println!("Sutract :{}",sub )
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use calculator;
+fn main() {
+    println!("simple calculator::");
+    let mut no1=20;
+    let mut no2=30;
+  calculator::sim_calculator::calculator1(no1,no2);
+  calculator::sim_calculator::calculator2(no1,no2);
+}
